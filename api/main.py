6e8fde6df6ddcd39b978d3d3cbbb9445b81bf063
Ivan Pereira <navi1921@gmail.com>
2021-12-11 20:16:41 -0300

    API funcionando

--- api/main.py
+++ api/main.py
@@ -1,16 +1,102 @@
+""" Módulo principal da API de predição
+"""
+import joblib
 from fastapi import FastAPI
+from pydantic import BaseModel, validator
+from binning import binning
+import numpy as np
+import xgboost
+import shap
+description = """
+# Desafio Machine Learning Engineer. 🚀
 
-app = FastAPI()
+API para predição do motivo de alta de pacientes, com base em:
+
+- idade
+- setor
+- temperatura
+- frequência respiratória
+- pressão arterial sistólica
+- pressão arterial diastólica
+- pressão arterial média
+- saturação da oxigenação
+
+O motivo de alta assumido é:
+- Óbito
+- Melhorado
+
+"""
+
+app = FastAPI(
+    title="PredictAPI",
+    description=description,
+    version="0.0.1",
+    contact={
+        "name": "Ivan Pereira",
+        "email": "navi1921@gmail.com",
+    },
+)
 
 
-def factorial(number):
-    if number == 0:
-        return 1
+
+model = joblib.load("models/xgb_model.joblib")
+label_encoder = joblib.load("models/le.joblib")
+scaler = joblib.load("models/scaler.joblib")
+
+nosection_model = joblib.load("models/nosection_xgb_model.joblib")
+nosection_scaler = joblib.load("models/nosection_scaler.joblib")
+
+@app.get("/predict")
+def predict_data(age: int,
+                sector: str,
+                temperature: float,
+                respiratory_frequency: float,
+                systolic_blood_pressure: float,
+                diastolic_blood_pressure: float,
+                mean_arterial_pressure: float,
+                oxygen_saturation: float
+                ):
+    """Informe os dados do paciente que deseja obter uma predição."""
+    if(sector != 'Nenhum'):
+        sector_encoded = label_encoder.transform([sector])
+        features = [
+            age,
+            sector_encoded.item(),
+            temperature,
+            respiratory_frequency,
+            systolic_blood_pressure,
+            diastolic_blood_pressure,
+            mean_arterial_pressure,
+            oxygen_saturation,
+        ]
     else:
-        return number * factorial(number - 1)
+        features = [
+            age,
+            0,
+            temperature,
+            respiratory_frequency,
+            systolic_blood_pressure,
+            diastolic_blood_pressure,
+            mean_arterial_pressure,
+            oxygen_saturation,
+        ]
 
-
-@app.get("/factorial")
-async def root(number: int):
-    """This is a endpoint to find the factorial of an integer"""
-    return {"answer": factorial(number)}
+    extra_features=binning(features)
+    features.extend(list(extra_features.flatten()))
+    
+    features_np=np.asarray(features,dtype=np.float64)
+    if(sector != 'Nenhum'):
+        features_np=scaler.transform([features_np])
+        y_pred = model.predict(features_np)
+        return {"predict": y_pred[0],'features': features}
+    else:
+        print(features_np)
+        features_np=np.delete(features_np, 1)
+        print(features_np)
+        features_np=nosection_scaler.transform([features_np])
+        y_pred = nosection_model.predict(features_np)
+        features=[features[0]]+features[2:]
+        print(features)
+        return {"predict": y_pred[0],'features': features}
+    
+    
